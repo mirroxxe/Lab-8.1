c2b1e3c0934a1cbf3fe8643afcd41d8fdb3ff43b
Volodya <chuchman002@gmail.com>
2023-11-30 23:14:11 +0200

    Add project files.

---
+++ Lab №8.1/Lab №8.1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+
+bool containsWhile(const std::string& input) {
+    std::string target = "while";
+
+    for (size_t i = 0; i < target.length(); ++i) {
+        bool found = false;
+        for (size_t j = 0; j < input.length(); ++j) {
+            if (target[i] == input[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void replaceWhile(std::string& input) {
+    size_t pos = input.find("while");
+
+    while (pos != std::string::npos) {
+        input.replace(pos, 5, "**");
+        pos = input.find("while", pos + 2);
+    }
+}
+
+int main() {
+    std::string inputString;
+
+    std::cout << "Print line: ";
+    std::getline(std::cin, inputString);
+
+    if (containsWhile(inputString)) {
+        std::cout << "String contains all letters from 'while'.\n";
+    }
+    else {
+        std::cout << "String doesn`t contain all letters with 'while'.\n";
+    }
+
+    replaceWhile(inputString);
+
+    std::cout << "Result: " << inputString << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab №8.1/Lab №8.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+
+		TEST_METHOD(TestContainsWhile_Negative)
+		{
+			std::string testInput = "randomtext"; 
+
+			// Перевіряємо, чи функція containsWhile повертає false для рядка, який не містить усі літери з "while"
+			Assert::IsFalse(containsWhile(testInput));
+		}
+	};
+}
